00fce6bb72370cce35e93ac65809ec2023a36f3e
Kurt Miebach <kwmiebach@gmail.com>
2011-06-28 23:52:28 +0200

    added example for local config

---
+++ config_local.example.php
@@ -0,0 +1,9 @@
+<?
+# DIESE DATEI UMBENENNEN IN config_local.php und eigene Werte setzen:
+
+$DEBUG_MESSAGES=TRUE;
+# Empfänger des Mailformulars (Das ist i.d.R. der Inhaber der Website)
+$MAIL_ADDRESS_OWNER="owner@example.com";
+$MAIL_ADDRESS_SERVER="server1@example.com";
+
+?>
